c9908766422d336d45f582c2b4a07a57f2b09cbe
Pedro Alexander Garcia A <pedroalexandergarciaa@Pedros-iMac.local>
2023-09-20 21:40:10 -0400

    if/else statement added

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,9 +3,12 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 //import icons
 import { BsPlus, BsEyeFill } from "react-icons/bs";
+// import cart context
+import { CartContext } from "../contexts/CartContext";
 
 const Product = ({ product }) => {
-  console.log(product);
+  const { addToCart } = useContext(CartContext);
+  // console.log(product);
   // destructure products
   const { id, image, category, title, price } = product;
   return (
@@ -36,7 +39,8 @@ const Product = ({ product }) => {
             p-2 flex flex-col items-center justify-center gap-y-2
             opacity-0 group-hover:opacity-100 transition-all duration-300"
           >
-            <button>
+            {/* Check to console to see if items are added it */}
+            <button onClick={() => addToCart(product, id)}>
               <div
                 className="flex justify-center items-center
               text-white w-12 h-12 bg-red-500"
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+// create context
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  // cart state
+  const [cart, setCart] = useState([]);
+
+  // add to cart
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    // check if the item is addded in the cart
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    // if cart item is already in the cart use in return (else statement)
+    // set cart to (newCart)
+    // if/else statement return item
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+  };
+
+
+  return (
+    <CartContext.Provider value={{ addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
 
-export default CartContext;
+export default CartProvider;
